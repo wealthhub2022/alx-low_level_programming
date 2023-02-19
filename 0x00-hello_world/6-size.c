be737d3833f8f05d812cfc449fc92ab10810f8d4
Abraham <abrahamwealthcon@gmail.com>
2023-02-19 10:03:54 -0800

    working

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,7 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)", sizeof(char));
-	printf("\n");
+	printf("Size of a char: %lu byte(s)", sizeof(char),"\n");
 	printf("Size of an int: %lu byte(s)", sizeof(int));
 	printf("\n");
 	printf("Size of a long int: %lu byte(s)", sizeof(long int));
