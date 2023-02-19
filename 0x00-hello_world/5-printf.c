1817db361d0043e2b16ceac08a0c036be118da81
Abraham <abrahamwealthcon@gmail.com>
2023-02-19 08:47:00 -0800

    working

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function of the program
+ *
+ * Return: this alwaysi return zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
