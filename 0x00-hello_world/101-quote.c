5c939d49a204b7b6cf36cf693fdcc2be516f03fe
Abraham <abrahamwealthcon@gmail.com>
2023-02-19 13:28:30 -0800

    working

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main -  printing out a msage
+ *
+ * Return: this will retutn 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
