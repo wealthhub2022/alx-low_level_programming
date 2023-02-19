6394e5f0e901fa8ca15a88ab182ad68a73fb131f
Abraham <abrahamwealthcon@gmail.com>
2023-02-19 07:41:08 -0800

    working

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - this is the main function
+ *
+ *Return: always zero 0
+ */
+int main(void)
+{
+	puts("\"ptogramming is like building a multilingual puzzle");
+	return (0);
+}
