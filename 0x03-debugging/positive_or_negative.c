8d485332b1683005f04add9a0f66506ada1739e3
Abraham <abrahamwealthcon@gmail.com>
2023-02-23 16:27:02 -0800

    working

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -11,7 +11,7 @@
 void positive_or_negative(int i)
 {
 	if (i < 0)
-		printf("%d is negetive", i);
+		printf("%d is negetive\n", i);
 	else
-		printf("%d is postive", i);
+		printf("%d is postive\n", i);
 }
