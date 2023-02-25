fa89e30d71f97851e2423c6d6724edfb2f05739d
Abraham <abrahamwealthcon@gmail.com>
2023-02-25 14:18:32 -0800

    working

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a letter is upper
+ * @x: The number to be checked 
+ * Return: 1 and 0
+ *
+ */
+
+int _isupper(int y)
+{
+	if(y >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
