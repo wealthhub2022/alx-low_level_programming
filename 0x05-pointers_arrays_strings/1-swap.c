bed73cc54d80dcf8be7657376a70dedabe6bcbd0
Abraham <abrahamwealthcon@gmail.com>
2023-02-28 14:25:07 -0800

    working

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * swap_int - this will swap the values
+ *
+ * @a: valua a
+ * @b: vlaue b
+ *
+ * Return: Always 0
+ *
+ *
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+
+}
