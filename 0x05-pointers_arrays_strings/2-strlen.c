aa3ff7861d7634a1a5783bbb99822d69ee9efa3d
Abraham <abrahamwealthcon@gmail.com>
2023-02-28 20:42:42 -0800

    working

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of 
+ * @s: the placse holder for
+ *
+ * Return: the value
+ */
+int  _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for(i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
